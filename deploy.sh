30a2d3d451653f99c3fd89500bfb751188717a23
tanklin <tank.lin@bigdata.com.tw>
2017-04-04 22:31:56 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 hexo generate
-cp -R public/* .deploy/tanklin.github.io
-cd .deploy/tanklin.github.io
+cp -R public/* .deploy_git/tanklin.github.io
+cd .deploy_git/tanklin.github.io
 git add .
-git commit -m “update”
+git commit -m "update"
 git push origin master
